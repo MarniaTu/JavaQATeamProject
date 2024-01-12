751f2449ebe225da41039d3f2dc896fa36df6dd9
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2024-01-12 14:16:45 +0300

    Fix #16

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -91,7 +91,7 @@ public class SavingAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        if (balance + amount < maxBalance) {
+        if (balance + amount <= maxBalance) {
             balance += amount;
             return true;
         } else {
