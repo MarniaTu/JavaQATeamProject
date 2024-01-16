5b17676f21475640ea701de3d36633ab1b3b1ca8
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2024-01-16 15:54:29 +0300

    correct Assertions in BankTest.java

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -13,21 +13,20 @@ public class BankTest {
 
         bank.transfer(from, to, 1_000);
 
-        Assertions.assertEquals(true, from.pay(1_000));
-        Assertions.assertEquals(true, to.add(1_000));
+        Assertions.assertEquals(5_000 - 1_000, from.getBalance());
+        Assertions.assertEquals(1_000 + 1_000, to.getBalance());
 
     }
 
     @Test
     public void shouldTransferFromCreditToSavingWithinCreditLimit() {
-        Account to = new SavingAccount(5_000, 0, 10_000, 12);
         Account from = new CreditAccount(1_000, 5_000, 17);
-
+        Account to = new SavingAccount(5_000, 0, 10_000, 12);
 
         bank.transfer(from, to, 2_000);
 
-        Assertions.assertEquals(true, from.pay(2_000));
-        Assertions.assertEquals(true, to.add(2_000));
+        Assertions.assertEquals(1_000 - 2_000, from.getBalance());
+        Assertions.assertEquals(5_000 + 2_000, to.getBalance());
 
     }
 
@@ -38,19 +37,19 @@ public class BankTest {
 
         bank.transfer(from, to, 0);
 
-        Assertions.assertEquals(false, from.pay(0));
-        Assertions.assertEquals(false, to.add(0));
+        Assertions.assertEquals(3_000, from.getBalance());
+        Assertions.assertEquals(1_000, to.getBalance());
     }
 
     @Test
     public void shouldNotTransferNegativeAmountFromCredit() {
-        Account to = new SavingAccount(3_000, 0, 10_000, 12);
         Account from = new CreditAccount(1_000, 5_000, 17);
+        Account to = new SavingAccount(3_000, 0, 10_000, 12);
 
         bank.transfer(from, to, -2_000);
 
-        Assertions.assertEquals(false, from.pay(-2_000));
-        Assertions.assertEquals(false, to.add(-2_000));
+        Assertions.assertEquals(1_000, from.getBalance());
+        Assertions.assertEquals(3_000, to.getBalance());
     }
 
     @Test
@@ -60,31 +59,31 @@ public class BankTest {
 
         bank.transfer(from, to, 2_500);
 
-        Assertions.assertEquals(false, from.pay(2_500));
-        Assertions.assertEquals(false, to.add(2_500));
+        Assertions.assertEquals(2_000, from.getBalance());
+        Assertions.assertEquals(1_000, to.getBalance());
     }
 
     @Test
     public void shouldNotTransferFromCreditToSavingAboveMaxBalance() {
-        Account to = new SavingAccount(7_000, 0, 10_000, 12);
         Account from = new CreditAccount(1_000, 5_000, 17);
+        Account to = new SavingAccount(7_000, 0, 10_000, 12);
 
         bank.transfer(from, to, 4_000);
 
-        Assertions.assertEquals(false, from.pay(4_000));
-        Assertions.assertEquals(false, to.add(4_000));
+        Assertions.assertEquals(1_000, from.getBalance());
+        Assertions.assertEquals(7_000, to.getBalance());
 
     }
 
     @Test
     public void shouldNotTransferFromCreditToSavingBelowCreditLimit() {
-        Account to = new SavingAccount(2_000, 0, 10_000, 12);
         Account from = new CreditAccount(2_000, 5_000, 17);
+        Account to = new SavingAccount(2_000, 0, 10_000, 12);
 
         bank.transfer(from, to, 7_500);
 
-        Assertions.assertEquals(false, from.pay(7_500));
-        Assertions.assertEquals(false, to.add(7_500));
+        Assertions.assertEquals(2_000, from.getBalance());
+        Assertions.assertEquals(2_000, to.getBalance());
 
     }
 }
